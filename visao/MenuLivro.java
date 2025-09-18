bcc2363b1740c7b41c7a7bb91eb9383c46eae32a
Gabriel Souza <gabrielsouza010208@gmail.com>
2025-09-18 15:51:01 -0300

    initial book menu

--- visao/MenuLivro.java
+++ visao/MenuLivro.java
@@ -1,5 +1,56 @@
 package visao;
 
+import java.util.Scanner;
+
+import modelo.Livro;
+import persistencia.BancoDeDados;
+import persistencia.IDInvalido;
+
 public class MenuLivro {
+	
+	public static void menuLivros(Scanner sc, BancoDeDados bd) {
+		int opcao = 0;
+		
+		do {
+			System.out.println("---------- MENU LIVROS ----------");
+			System.out.println("1- Adicionar Livro");
+			System.out.println("2- Remover Livro");
+			System.out.println("3- Alterar Livro pelo ID");
+			System.out.println("4- Pesquisar Livro pelo ID");
+			System.out.println("5- Listar todos os Livros");
+			System.out.println("0- Voltar ao Menu Principal");
+			
+			opcao = sc.nextInt();
+			sc.nextLine();
+			
+			switch(opcao) {
+			case 1:
+				System.out.println("Informe os dados do livro");
+				System.out.println("Nome: ");
+				String nomeLivro = sc.nextLine();
+				System.out.println("Autor: ");
+				String autorLivro = sc.nextLine();
+				System.out.println("ID: ");
+				int idLivro = sc.nextInt();
+				Livro livro = new Livro(nomeLivro, autorLivro, idLivro);
+				bd.getLivros().adicionar(livro);
+				System.out.println("Livro adicionado.");
+				break;
+			case 2:
+				try {
+					System.out.println("Informe o ID do Livro a ser removido: ");
+					int idRemocao = sc.nextInt();
+					bd.getLivros().remover(idRemocao);
+					System.out.println("Livro removido.");
+					break;
+				} catch (IDInvalido e) {
+					System.out.println("Erro: ID invalido. ");
+					break;
+				}
+			case 3:
+				break;
+			}
+		} while (opcao != 0);
+	}
 
 }
